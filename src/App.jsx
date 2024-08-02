39f2ef2826957069f792dc6d19e4e66a084642ed
prasetyodimasp <prasetyodimasp2711@gmail.com>
2024-08-02 18:34:21 +0700

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="max-w-4xl m-auto relative">
+            <nav className="fixed left-0 right-0 top-0 z-20" id="home">
+                <div className="container m-auto px-4 py-6 max-w-4xl bg-black">
+                    <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+                        <div>
+                            <a
+                                href="#name"
+                                className="font-bold text-2xl text-blue-500 cursor-pointer"
+                            >
+                                Prasetyodimas.
+                            </a>
+                        </div>
+                        <div>
+                            <ul className="flex gap-5">
+                                <li>
+                                    <a
+                                        href="#skills"
+                                        className="text-gray-400 hover:text-blue-500 cursor-pointer"
+                                    >
+                                        Skills
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#portofolio"
+                                        className="text-gray-400 hover:text-blue-500 cursor-pointer"
+                                    >
+                                        Portofolio
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#aboutme"
+                                        className="text-gray-400 hover:text-blue-500 cursor-pointer"
+                                    >
+                                        Education
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#saya"
+                                        className="text-gray-400 hover:text-blue-500 cursor-pointer"
+                                    >
+                                        Hubungi Saya
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
-export default App
+export default App;
