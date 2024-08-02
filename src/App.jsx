126541412784eae992b4041cfbcb76fb3726bc94
prasetyodimasp <prasetyodimasp2711@gmail.com>
2024-08-02 18:59:18 +0700

    Add Aducation

--- src/App.jsx
+++ src/App.jsx
@@ -280,6 +280,41 @@ function App() {
                         </div>
                     </div>
                 </section>
+                {/* Education */}
+                <section className="py-8" id="aboutme">
+                    <div className="container m-auto px-4">
+                        <h2 className="text-2xl font-semibold text-blue-500">
+                            Education
+                        </h2>
+                        <div className="mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-10 sm:before:bottom-2 before:w-1 before:bg-white">
+                            <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+                                <h3 className="absolute left-0 text-lg font-semibold">
+                                    2023
+                                </h3>
+                                <p>
+                                    Lulus Dari SMA Negeri 1 Wonogiri, Jawa
+                                    Tengah, Indonesia.
+                                </p>
+                            </div>
+                            <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+                                <h3 className="absolute left-0 text-lg font-semibold">
+                                    2024
+                                </h3>
+                                <p>
+                                    Lulus Dari Salah Satu Bootcamp Fullstack Web
+                                    Developer &nbsp;
+                                    <a
+                                        className="text-blue-600"
+                                        href="https://www.harisenin.com/"
+                                        target="_blank"
+                                    >
+                                        Harisenin.com
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </div>
     );
